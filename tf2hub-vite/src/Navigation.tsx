0d250fce299897b80a9924ef9076577d31c7f533
Adam Swanson <adam.swanson.98@gmail.com>
2023-09-12 21:04:42 -0400

    Add mobile navigation

--- tf2hub-vite/src/Navigation.tsx
+++ tf2hub-vite/src/Navigation.tsx
@@ -1,9 +1,40 @@
 import { NavLink } from "react-router-dom";
 import "./Navigation.scss";
+import {
+  Box,
+  Button,
+  Divider,
+  Drawer,
+  List,
+  ListItem,
+  ListItemButton,
+  ListItemText,
+} from "@mui/material";
+import { useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars } from "@fortawesome/free-solid-svg-icons";
 
 function Navigation() {
+  const [drawerOpen, setDrawerOpen] = useState(false);
+
+  const toggleDrawer =
+    (open: boolean) => (event: React.KeyboardEvent | React.MouseEvent) => {
+      if (
+        event.type === "keydown" &&
+        ((event as React.KeyboardEvent).key === "Tab" ||
+          (event as React.KeyboardEvent).key === "Shift")
+      ) {
+        return;
+      }
+
+      setDrawerOpen(open);
+    };
+
   return (
     <div className="navigation">
+      <Button className="hamburger-btn" onClick={toggleDrawer(true)}>
+        <FontAwesomeIcon icon={faBars} size="xl" />
+      </Button>
       <div className="title">
         <span className="title">TF2 Hub</span>
         <img
@@ -19,6 +50,53 @@ function Navigation() {
           Become a Sponsor
         </a>
       </div>
+      <div className="spacer"></div>
+      <Drawer
+        className="mobile-nav"
+        open={drawerOpen}
+        onClose={toggleDrawer(false)}
+      >
+        <Box
+          sx={{ width: 250 }}
+          role="presentation"
+          onClick={toggleDrawer(false)}
+          onKeyDown={toggleDrawer(false)}
+        >
+          <List>
+            <NavLink to={"/"}>
+              <ListItem disablePadding>
+                <ListItemButton>
+                  <ListItemText primary="Home" />
+                </ListItemButton>
+              </ListItem>
+            </NavLink>
+            <NavLink to={"/cleantf2"}>
+              <ListItem disablePadding>
+                <ListItemButton>
+                  <ListItemText primary="CleanTF2" />
+                </ListItemButton>
+              </ListItem>
+            </NavLink>
+            <NavLink to={"/trashcan"}>
+              <ListItem disablePadding>
+                <ListItemButton>
+                  <ListItemText primary="Trashcan" />
+                </ListItemButton>
+              </ListItem>
+            </NavLink>
+          </List>
+          <Divider />
+          <List>
+            <a href="https://github.com/sponsors/theaswanson" target="_blank">
+              <ListItem disablePadding>
+                <ListItemButton>
+                  <ListItemText primary="Become a Sponsor" />
+                </ListItemButton>
+              </ListItem>
+            </a>
+          </List>
+        </Box>
+      </Drawer>
     </div>
   );
 }
